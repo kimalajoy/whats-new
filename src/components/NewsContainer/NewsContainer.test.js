0b36de738a0701585fbea8369ba37d70847ab134
Kimala Cochran <kimalajoy@gmail.com>
2020-05-07 18:58:02 -0600

    completed unit test for newsArticle

---
+++ src/components/NewsContainer/NewsContainer.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NewsArticle from './NewsArticle'
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('NewsArticle', () => {
+  it('renders correctly', () => {
+    const {getByText} = render(
+    <NewsArticle 
+    headline='This is the heading'
+    url="http://www.google.com"
+    description='This is the description'
+    />)
+
+    expect(getByText('This is the heading')).toBeInTheDocument();
+        expect(getByText('This is the description')).toBeInTheDocument();
+  });
+});
